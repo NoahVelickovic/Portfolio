203552a6615fc24443897fa53dfd9544bc4b2bc5
Noah Velickovic <noah.velic@hotmail.com>
2025-09-26 22:39:29 +0200

    Hero section, Why me section and English/German translation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,41 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled',
+      })
+    ),
+
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+
+    // ✅ Moderne ngx-translate-Konfiguration (Standalone + Provider-APIs)
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json',
+      }),
+      // setDefaultLang ist deprecated → nutze "fallbackLang" + "lang"
+      fallbackLang: 'de',
+      lang: 'de',
+    }),
+  ],
 };
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { get } from 'http';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, TranslatePipe],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  current = 'de';
 
+  constructor(private translate: TranslateService) {
+    this.current = this.translate.currentLang || 'de';
+  }
+
+  switchLang(lang: 'de' | 'en') {
+    this.current = lang;
+    this.translate.use(lang);
+}
 }
