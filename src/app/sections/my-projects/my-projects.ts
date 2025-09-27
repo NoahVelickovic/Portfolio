4ebee674d0fde96d66b390801f211df74bfeb68f
Noah Velickovic <noah.velic@hotmail.com>
2025-09-27 11:26:45 +0200

    angular update commit

--- src/app/sections/my-projects/my-projects.ts
+++ src/app/sections/my-projects/my-projects.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { ProjectOne } from '../projects/project-one/project-one';
+import { ProjectTwo } from '../projects/project-two/project-two';
+import { ProjectThree } from '../projects/project-three/project-three';
 
 @Component({
   selector: 'app-my-projects',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './my-projects.html',
   styleUrl: './my-projects.scss'
 })
