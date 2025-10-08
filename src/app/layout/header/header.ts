6fb73ab427129e80fe8a47eb211888b9b3ed48f1
Noah Velickovic <noah.velic@hotmail.com>
2025-10-08 17:23:51 +0200

    local storage language and bug fixes

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,33 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
-import { get } from 'http';
-
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [RouterLink, TranslatePipe],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
 export class Header {
-  current = 'de';
+  private readonly isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+  current: 'de' | 'en' = 'de';
 
   constructor(private translate: TranslateService) {
-    this.current = this.translate.currentLang || 'de';
+    if (this.isBrowser) {
+      const savedLang = localStorage.getItem('app-lang') as 'de' | 'en' | null;
+      if (savedLang) {
+        this.current = savedLang;
+        this.translate.use(savedLang);
+      }
+    }
   }
 
-  switchLang(lang: 'de' | 'en') {
+  setLang(lang: 'de' | 'en') {
     this.current = lang;
     this.translate.use(lang);
-}
+    if (this.isBrowser) {
+      localStorage.setItem('app-lang', lang);
+    }
+  }
 
   toggleMenu() {
-    const burgerBox = document.querySelector("#burger-box")
+    const burgerBox = document.querySelector('#burger-box');
     const burgerMenu = document.querySelector('#menu');
     const headerRight = document.querySelector('#header-right');
     burgerMenu?.classList.toggle('burger-menu-active');
-    headerRight?.classList.toggle("flex")
+    headerRight?.classList.toggle('flex');
     burgerBox?.classList.toggle('active-burger');
   }
 }
